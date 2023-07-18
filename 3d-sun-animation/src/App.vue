ff19d050d983fa30e5a21f98f4cf0b73d613e6b9
wangjiny21 <2945979072@qq.com>
2023-07-18 17:24:01 +0800

    Feat sideview&verticalview

--- 3d-sun-animation/src/App.vue
+++ 3d-sun-animation/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/sideview">SideView</router-link> |
+    <router-link to="/verticalview">VerticalView</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
