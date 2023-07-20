f71aab8a13f14f9c3c422f8cb548e7de91ef8b33
wangjiny21 <2945979072@qq.com>
2023-07-20 17:09:44 +0800

    Feat infos

--- 3d-sun-animation/src/views/SideView.vue
+++ 3d-sun-animation/src/views/SideView.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="sideview">
+    <div id="navbar">
+      <a id="toggle-data" href="#data"><i class="icon-data"></i>Data</a>
+    </div>
+    <div id="data">
+      <a class="sun" title="sun" href="#sunspeed">Sun</a>
+      <a class="mercury" title="mercury" href="#mercuryspeed">Mercury</a>
+      <a class="venus active" title="venus" href="#venusspeed">Venus</a>
+      <a class="earth" title="earth" href="#earthspeed">Earth</a>
+      <a class="mars" title="mars" href="#marsspeed">Mars</a>
+      <a class="jupiter" title="jupiter" href="#jupiterspeed">Jupiter</a>
+      <a class="saturn" title="saturn" href="#saturnspeed">Saturn</a>
+      <a class="uranus" title="uranus" href="#uranusspeed">Uranus</a>
+      <a class="neptune" title="neptune" href="#neptunespeed">Neptune</a>
+    </div>
     <div id="universe" class="scale-stretched">
       <div id="galaxy">
         <div id="solar-system" class="earth">
--- 3d-sun-animation/src/views/VerticalView.vue
+++ 3d-sun-animation/src/views/VerticalView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="verticalview">
+    <div id="data">
+      <a class="sun" title="sun" href="#sunspeed">Sun</a>
+      <a class="mercury" title="mercury" href="#mercuryspeed">Mercury</a>
+      <a class="venus active" title="venus" href="#venusspeed">Venus</a>
+      <a class="earth" title="earth" href="#earthspeed">Earth</a>
+      <a class="mars" title="mars" href="#marsspeed">Mars</a>
+      <a class="jupiter" title="jupiter" href="#jupiterspeed">Jupiter</a>
+      <a class="saturn" title="saturn" href="#saturnspeed">Saturn</a>
+      <a class="uranus" title="uranus" href="#uranusspeed">Uranus</a>
+      <a class="neptune" title="neptune" href="#neptunespeed">Neptune</a>
+    </div>
     <div id="universe" class="scale-stretched">
       <div id="galaxy">
         <div id="solar-system" class="earth">
